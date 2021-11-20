614dafaf7af0a08f7a8eef5a46b4f8bcdd580531
rakibemon <rakibemon03@gmail.com>
2021-11-20 23:51:51 +0600

    modify service data

--- src/components/ServiceDetail/ServiceDetail.js
+++ src/components/ServiceDetail/ServiceDetail.js
@@ -1,5 +1,5 @@
 import { Button } from '@mui/material';
-// import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory, useParams } from 'react-router';
 import './ServiceDetail.css'
 const ServiceDetail = () => {
@@ -9,32 +9,31 @@ const ServiceDetail = () => {
         history.push('/services')
     };
 
-    // const [serviceDetails, setServiceDetail] = useState([]);
-    // useEffect(() => {
-    //     fetch('/serviceDetails.json')
-    //         .then(res => res.json())
-    //         .then(data => setServiceDetail(data))
-    // }, []);
+    const [serviceDetails, setServiceDetail] = useState([]);
+    useEffect(() => {
+        fetch('/serviceDetails.json')
+            .then(res => res.json())
+            .then(data => setServiceDetail(data))
+    }, []);
 
-    // const [singleCourse, setSingleCourse] = useState({})
+    const [singleCourse, setSingleCourse] = useState({})
 
     // find out the service that matched
-    // useEffect(() => {
-    //     const foundCourse = serviceDetails.find(course => course.id === parseInt(serviceId));
-    //     setSingleCourse(foundCourse)
-    // }, [serviceDetails,serviceId]);
+    useEffect(() => {
+        if(serviceDetails.length){
+            const foundCourse = serviceDetails.find(course => course.id === parseInt(serviceId));
+            setSingleCourse(foundCourse)
+        }
+    }, [serviceDetails,serviceId]);
     
-    // const {name,img,description,price} =singleCourse;
+    const {name,img,description,price} =singleCourse || {};
     return (
         <div className='single-course-detail'>
             <div className='single-course-info'>
-                {/* <img src={img} alt="" />
+                <img src={img} alt="" />
                 <h4>{name}</h4>
                 <p>{description}</p>
-                <p>$ {price}</p> */}
-
-                <p> Course Id: {serviceId}</p>
-                <h6> Got some error We are working on Course Details. Soon you will be updated. Thanks for your patient</h6>
+                <p>Price : <b style={{color:'tomato'}}>$ {price}</b></p>
                 <Button onClick={goServices} variant='contained'> Go Services </Button>
             </div>
         </div>
